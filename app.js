c3506a71ffc480a19db761bd6674d4744fff2e7b
andrepeter <avpetersheim0@frostburg.edu>
2019-03-26 17:40:34 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/documents', (req, res) => res.send('Sent a get request to documents'))
+app.post('/', (req, res) => res.send('Post request at root'))
+app.put('/', (req, res) => res.send('Got a put request at /user'))
+app.patch('/', (req, res) => res.send('Got a patch request at /user'))
+app.delete('/', (req, res) => res.send('Got a delete request at /user'))
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
