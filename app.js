74a534e7ccfef7c69a53ad2175aaa9b5b4a9c0ef
andrepeter <avpetersheim0@frostburg.edu>
2019-03-30 17:26:26 -0400

    Added full functionality to post request in app.js

--- app.js
+++ app.js
@@ -3,13 +3,30 @@ var mysql = require('mysql')
 const app = express()
 const port = 3000
 
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "andrepeter",
+    password: "GoinDownFerReal567",
+    database: "mydatabase"
+});
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/documents', (req, res) => res.send('Sent a get request to documents'))
-app.post('/', (req, res) => res.send('Post request at root'))
+
+app.get('/students', (req, res) => res.send('Hello World!'))
+
+app.post('/create-student', (req, res) => {
+    con.connect(function(err){
+        if (err) throw err;
+        console.log("Connected!");
+        var sql = "INSERT INTO students (id, firstName, lastName, year, age, major, gpa) VALUES ('12', 'Andre', 'Petersheim', '2021', '19', 'CS', '4.0')";
+        con.query(sql, function(err, result){
+            if(err) throw err;
+            res.send("1 record inserted");
+        });
+    });
+})
+
 app.put('/', (req, res) => res.send('Got a put request at /user'))
 app.patch('/', (req, res) => res.send('Got a patch request at /user'))
 app.delete('/', (req, res) => res.send('Got a delete request at /user'))
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
